9feb08ad9ccbf271d49b8e0d73f03489e2f783e1
Djeisen642 <Djeisen642@users.noreply.github.com>
2026-07-17 10:43:16 +0000

    deploy: bf0dcbc1315bedbe5c590b9e3852e9f5291e046b

---
+++ _nuxt/CkUpKgOp.js
@@ -0,0 +1,21 @@
+import{h as a,q as c,I as r,B as t,ag as C,R as l}from"./CsXSmiCK.js";const d=["width","height"],u=a({__name:"BGCLogoIcon",props:{size:{default:24},color:{default:"#C8860A"}},setup(n){const e=n,s=l(()=>Math.round(e.size*.8)),i=l(()=>e.color);return(h,o)=>(c(),r("svg",{width:s.value,height:n.size,viewBox:"0 0 80 100",xmlns:"http://www.w3.org/2000/svg","aria-hidden":"true",style:C({display:"inline-block",verticalAlign:"middle",fill:i.value})},[...o[0]||(o[0]=[t("circle",{cx:"40",cy:"14",r:"13"},null,-1),t("path",{d:`M 40 27
+         C 46 27, 60 29, 66 31
+         C 70 32, 75 34, 76 38
+         C 77 43, 72 46, 66 45
+         C 60 44, 54 44, 51 46
+         C 52 52, 54 60, 54 72
+         L 54 83
+         C 54 89, 51 93, 47 93
+         L 43 93
+         L 43 68
+         L 37 68
+         L 37 93
+         L 33 93
+         C 29 93, 26 89, 26 83
+         L 26 72
+         C 26 60, 28 52, 29 46
+         C 26 44, 20 44, 14 45
+         C 8 46, 3 43, 4 38
+         C 5 33, 10 32, 14 31
+         C 20 29, 34 27, 40 27
+         Z`},null,-1)])],12,d))}}),p=Object.assign(u,{__name:"BGCLogoIcon"});export{p as _};
